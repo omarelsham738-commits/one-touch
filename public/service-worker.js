3f1b7d8a94dc9cee01bf199d2cc58832a9f9cd3b
omarelsham738-commits <omarelsham738@gmail.com>
2026-08-04 23:57:26 +0300

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const CACHE = 'one-touch-v1';
+const urls = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(urls)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
